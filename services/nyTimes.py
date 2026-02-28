6739ecc4c4bf2c4136cd4a1db9cc085c3fcc0ada
Gureet Sidhu <42849884+gs109111@users.noreply.github.com>
2026-02-28 14:01:26 -0700

    Update log messages for WordleSolver/NYTimes

--- services/nyTimes.py
+++ services/nyTimes.py
@@ -14,12 +14,12 @@ def main():
     print("")
     print("")
 
-    cprint("[nytimes-wordle-service] Starting Timer...", color="blue", attrs=['bold'])
+    cprint("[WordleSolver/NYTimes] Starting Timer...", color="blue", attrs=['bold'])
     print("")
 
     start_time = time.time()
 
-    cprint("[nytimes-wordle-service] Getting Current Date...", color="blue", attrs=['bold'])
+    cprint("[WordleSolver/NYTimes] Getting Current Date...", color="blue", attrs=['bold'])
 
     year = str(datetime.datetime.now().year)
     month = str(datetime.datetime.now().month)
@@ -29,14 +29,14 @@ def main():
         month = f"0{month}"
 
     print("")
-    cprint("[nytimes-wordle-service] Loading Wordle Data From NYTimes...", color="blue", attrs=['bold'])
+    cprint("[WordleSolver/NYTimes] Loading Wordle Data From NYTimes...", color="blue", attrs=['bold'])
 
     print("")
     url = f"https://www.nytimes.com/svc/wordle/v2/{year}-{month}-{day}.json"
     req = requests.get(url)
     json = req.json()
 
-    cprint("[nytimes-wordle-service] Parsing & Displaying Results...", color="blue", attrs=['bold'])
+    cprint("[WordleSolver/NYTimes] Parsing & Displaying Results...", color="blue", attrs=['bold'])
 
     solution = json["solution"]
     solution = solution[0].upper() + solution[1:]
@@ -46,28 +46,28 @@ def main():
 
     print("")
 
-    cprint(f"[nytimes-wordle-service] ID: {id}", color="green", attrs=['bold'])
+    cprint(f"[WordleSolver/NYTimes] ID: {id}", color="green", attrs=['bold'])
 
     print("")
 
-    cprint(f"[nytimes-wordle-service] Print Date: {print_date}", color="green", attrs=['bold'])
+    cprint(f"[WordleSolver/NYTimes] Print Date: {print_date}", color="green", attrs=['bold'])
 
     print("")
 
-    cprint(f"[nytimes-wordle-service] Editor: {editor}", color="green", attrs=['bold'])
+    cprint(f"[WordleSolver/NYTimes] Editor: {editor}", color="green", attrs=['bold'])
 
     print("")
 
-    cprint(f"[nytimes-wordle-service] Answer: {solution}", color="white", attrs=['bold'])
+    cprint(f"[WordleSolver/NYTimes] Answer: {solution}", color="white", attrs=['bold'])
 
     print("")
 
-    cprint("[nytimes-wordle-service] Ending Timer...", color="blue", attrs=['bold'])
+    cprint("[WordleSolver/NYTimes] Ending Timer...", color="blue", attrs=['bold'])
 
     end_time = time.time()
 
     print("")
 
-    cprint(f"[nytimes-wordle-service] Found In: {round(end_time - start_time, 3)}s", color="yellow", attrs=['bold'])
+    cprint(f"[WordleSolver/NYTimes] Found In: {round(end_time - start_time, 3)}s", color="yellow", attrs=['bold'])
 
     print("")
