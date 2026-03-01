aa2a9803ed4c9f8f8dfe6055af04fff267c9aa5a
Gureet Sidhu <42849884+gs109111@users.noreply.github.com>
2026-03-01 09:18:49 -0700

    Added Word Length Filter...

--- filter_words.py
+++ filter_words.py
@@ -1,1 +1,5 @@
-# Filter words from a word list
+# Filter words that are not 5 letters
+with open("words.txt", "r") as f, open("words_filtered.txt", "w") as f2:
+    for line in f:
+        if len(line.strip()) == 5:
+            f2.write(line)
